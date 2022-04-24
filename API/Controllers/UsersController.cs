e366a50f560c683a816a3329306bd2df5238638b
Gavin <Gavinszczesniak@gmail.com>
2022-04-24 14:52:18 -0400

    Using jwt to give authentication and added extentions to use less repeating code.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,6 +15,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
         {
             var users = await _context.Users.ToListAsync();
@@ -22,6 +24,7 @@ namespace API.Controllers
         }
 
         // api/users/:id
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUsers(int id) 
         {
