56d21a6cd7bb2e16e3b9a120a7476b46bb292bb3
Gavin <Gavinszczesniak@gmail.com>
2022-04-25 19:26:36 -0400

    Added Repository and updated out database to incorporate more information (age, lastactive, profile picture, and more). We updated files within Extentions, Entities, and Controllers.

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -38,8 +38,8 @@ namespace API.Entities
 
         public ICollection<Photo> Photos { get; set; }
 
-        public int GetAge() {
-            return DateOfBirth.CalculateAge();
-        }
+        // public int GetAge() {
+        //     return DateOfBirth.CalculateAge();
+        // }
     }
 }
